b77f78db35935e79b9573ea505d08b52c4e672d6
Marius Busch <busch.marius@web.de>
2019-07-23 20:23:31 +0200

    Listener Anfang

--- Tutorial/src/de/Malebu/tutorial/listeners/JoinListener.java
+++ Tutorial/src/de/Malebu/tutorial/listeners/JoinListener.java
@@ -1,7 +1,14 @@
 package de.Malebu.tutorial.listeners;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinListener implements Listener {
+	
+	@EventHandler
+	public void handlePlayerJoin(PlayerJoinEvent event) {
+		
+	}
 
 }
