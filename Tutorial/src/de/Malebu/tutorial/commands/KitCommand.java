79282ee135c4baf8aa9a9b65f0a01a8a3b6b3243
Marius Busch <busch.marius@web.de>
2019-07-24 20:40:24 +0200

    Fehler behoben

--- Tutorial/src/de/Malebu/tutorial/commands/KitCommand.java
+++ Tutorial/src/de/Malebu/tutorial/commands/KitCommand.java
@@ -1,9 +1,11 @@
 package de.Malebu.tutorial.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 public class KitCommand implements CommandExecutor{
 
@@ -12,11 +14,23 @@ public class KitCommand implements CommandExecutor{
 		
 		if(sender instanceof Player) { 
 			Player player = (Player) sender;
+			if(player.hasPermission("Tutorial.kit")) {
+				if (args.length == 0) {
+					
+					
+			   
+					
+			} else
+				player.sendMessage("§c Bitte benutze /kit [SPIELER] §c");
+				
+		} else 
+			player.sendMessage("§cDazu hast du keine Rechte");	
 		
-		}
 		
-		return false;
 		
 	}
+		return false;
 
+  } 
+	
 }
