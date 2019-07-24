4b09797fac331e7f557a975f62a0c554af59eda2
Marius Busch <busch.marius@web.de>
2019-07-24 19:48:27 +0200

    PlguinManager erstellt, um den JoinListener zu managen

--- Tutorial/src/de/Malebu/tutorial/listeners/JoinListener.java
+++ Tutorial/src/de/Malebu/tutorial/listeners/JoinListener.java
@@ -1,5 +1,6 @@
 package de.Malebu.tutorial.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -8,6 +9,7 @@ public class JoinListener implements Listener {
 	
 	@EventHandler
 	public void handlePlayerJoin(PlayerJoinEvent event) {
+		event.setJoinMessage("§a Der Spieler §6" + event.getPlayer().getName() + "§a hat denServer betreten");
 		
 	}
 
--- Tutorial/src/de/Malebu/tutorial/main/Main.java
+++ Tutorial/src/de/Malebu/tutorial/main/Main.java
@@ -1,5 +1,7 @@
 package de.Malebu.tutorial.main;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import de.Malebu.tutorial.commands.HealCommand;
@@ -8,6 +10,8 @@ public class Main extends JavaPlugin {
 	
 	public void onEnable() {
 		getCommand("Heal").setExecutor(new HealCommand());
+		
+		PluginManager pluginMananger = Bukkit.getPluginManager();
 	}
 	
 }
