1676dca18bc2cc1a78d5a9de09d5564ad9a057cf
Marius Busch <busch.marius@web.de>
2019-07-24 20:04:01 +0200

    Video Listener abgeschlossen

--- Tutorial/src/de/Malebu/tutorial/main/Main.java
+++ Tutorial/src/de/Malebu/tutorial/main/Main.java
@@ -5,13 +5,15 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import de.Malebu.tutorial.commands.HealCommand;
+import de.Malebu.tutorial.listeners.JoinListener;
 
 public class Main extends JavaPlugin {
 	
 	public void onEnable() {
 		getCommand("Heal").setExecutor(new HealCommand());
 		
-		PluginManager pluginMananger = Bukkit.getPluginManager();
+		PluginManager pluginManager = Bukkit.getPluginManager();
+		pluginManager.registerEvents(new JoinListener(), this);
 	}
 	
 }
