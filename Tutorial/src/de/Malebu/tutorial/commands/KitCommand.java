05c6ab8518629ac5dda465a0107d15dc7b0a893e
Marius Busch <busch.marius@web.de>
2019-07-24 20:32:01 +0200

    Der Spieler wird geprüft und es wird festgelegt dass der Spieler der commitor ist.

--- Tutorial/src/de/Malebu/tutorial/commands/KitCommand.java
+++ Tutorial/src/de/Malebu/tutorial/commands/KitCommand.java
@@ -1,5 +1,22 @@
 package de.Malebu.tutorial.commands;
 
-public class KitCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class KitCommand implements CommandExecutor{
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+		if(sender instanceof Player) { 
+			Player player = (Player) sender;
+		
+		}
+		
+		return false;
+		
+	}
 
 }
