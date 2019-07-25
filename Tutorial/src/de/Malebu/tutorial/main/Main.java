898eb30076ceaf1596e92dfbebcd41cbbf14cfd8
Marius Busch <busch.marius@web.de>
2019-07-25 19:12:29 +0200

    Beenden des Kit-Inventars

--- Tutorial/src/de/Malebu/tutorial/main/Main.java
+++ Tutorial/src/de/Malebu/tutorial/main/Main.java
@@ -5,12 +5,14 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import de.Malebu.tutorial.commands.HealCommand;
+import de.Malebu.tutorial.commands.KitCommand;
 import de.Malebu.tutorial.listeners.JoinListener;
 
 public class Main extends JavaPlugin {
 	
 	public void onEnable() {
 		getCommand("Heal").setExecutor(new HealCommand());
+		getCommand("Kit").setExecutor(new KitCommand());
 		
 		PluginManager pluginManager = Bukkit.getPluginManager();
 		pluginManager.registerEvents(new JoinListener(), this);
